6db8120885e70938bba1690b3e9fbefc94de6414
20020450 <tranhainam1410@gmail.com>
2023-09-06 14:32:26 +0700

    first backend

---
+++ backend/backend/Data/SoundCloudContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace backend.Data
+{
+    public class SoundCloudContext : IdentityDbContext<IdentityUser>
+    {
+        public SoundCloudContext(DbContextOptions<SoundCloudContext> opt) : base(opt) { }
+
+        #region
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        #endregion
+    }
+}
